d6818e9a8f9bb9d868fa1796b01242b1970fd4e9
Lech Birek <lech.birek@eonnext.com>
2024-12-04 11:34:57 +0000

    Refactored day 1 solution and added tests

--- src/advent_of_code/y_2024/day_01/puzzle.py
+++ src/advent_of_code/y_2024/day_01/puzzle.py
@@ -1,15 +1,17 @@
-def read_data(input_path: str):
+def read_data(input_path: str) -> tuple[list[int], list[int]]:
     with open(input_path, "r") as inputs:
         all_lines = inputs.readlines()
-    return [[int(number) for number in line.split(' ')] for line in all_lines]
+    row_1 = [int(line.split("   ")[0]) for line in all_lines]
+    row_2 = [int(line.split("   ")[1]) for line in all_lines]
+    return (row_1, row_2)
 
-def main():
-    with open("src/advent_of_code/2024/day_01/input.txt", "r") as inputs:
-        all_lines = inputs.readlines()
-    list_1 = sorted([int(line.split("   ")[0]) for line in all_lines])
-    list_2 = sorted([int(line.split("   ")[1]) for line in all_lines])
-    print(distance(list_1, list_2))
-    print(similarity(list_1, list_2))
+def puzzle(row_1, row_2, part: int) -> int:
+    list_1 = sorted(row_1)
+    list_2 = sorted(row_2)
+    if part == 1:
+        return distance(list_1, list_2)
+    else:
+        return similarity(list_1, list_2)
 
 def distance(list_1: list[int], list_2: list[int]) -> int:
     return sum([abs(el_1 - el_2) for el_1, el_2 in zip(list_1, list_2)])
@@ -18,8 +20,8 @@ def similarity(list_1: list[int], list_2: list[int]) -> int:
     return sum([num * list_2.count(num) for num in list_1 if num in list_2])    
 
 if __name__ == "__main__":
-    data = read_data("src/advent_of_code/y_2024/day_02/input.txt")
-    result_no_damp = puzzle(data, 1)
-    result_damp = puzzle(data, 2)
-    print(f"Day 2, part 1: {result_no_damp}")
-    print(f"Day 2, part 2: {result_damp}")
+    data_1, data_2 = read_data("src/advent_of_code/y_2024/day_01/input.txt")
+    distance = puzzle(data_1, data_2, 1)
+    similarity = puzzle(data_1, data_2, 2)
+    print(f"Day 1, part 1: {distance}")
+    print(f"Day 1, part 2: {similarity}")
