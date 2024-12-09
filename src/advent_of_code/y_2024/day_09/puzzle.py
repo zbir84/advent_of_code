23420aef0f17c177b8dcc432528ac2e75bf394aa
Lech Birek <lech.birek@eonnext.com>
2024-12-09 13:53:40 +0000

    Solution for day 9 pt 1

--- src/advent_of_code/y_2024/day_09/puzzle.py
+++ src/advent_of_code/y_2024/day_09/puzzle.py
@@ -1,17 +1,58 @@
+import copy
+import time
+
+
 def read_data(input_path: str) -> list[list[str]]:
     with open(input_path, "r") as inputs:
-        lines = inputs.readlines()
-    return [[char for char in line.strip()] for line in lines]
+        input = inputs.read()
+    decoded_input = []
+    for i, char in enumerate(input):
+        if i % 2 == 0:
+            char_to_put = f"{int(i/2)}"
+        else:
+            char_to_put = "."
+        decoded_input += [char_to_put] * int(char)
+    return decoded_input
 
 
-def puzzle(data: list[list[str]]) -> tuple[int, int]:
-    answer_1 = 0
-    answer_2 = 0
-    return answer_1, answer_2
+def puzzle_pt_1(data: list[str]) -> tuple[int, int]:
+    ordered_list = order_the_list(data)
+    checksum = count_checksum(ordered_list)
+    return checksum
+
+
+def order_the_list(unordered_list):
+    j = len(unordered_list) - 1
+    i = 0
+    while i < j:
+        if unordered_list[i] == ".":
+            unordered_list[i] = unordered_list[j]
+            unordered_list[j] = "."
+            while unordered_list[j] == ".":
+                j -= 1
+        i += 1
+    return unordered_list[0 : j + 1]
+
+
+def puzzle_pt_2(data: list[str]) -> tuple[int, int]:
+    ordered_list = order_the_list_files(data)
+    checksum = count_checksum(ordered_list)
+    return checksum
+
+
+def order_the_list_files(unordered_list):
+    return ["0"]
+
+
+def count_checksum(ordered_list: list[str]) -> int:
+    return sum([i * int(id) for i, id in enumerate(ordered_list)])
 
 
 if __name__ == "__main__":
     data = read_data("src/advent_of_code/y_2024/day_09/input.txt")
-    pt_1, pt_2 = puzzle(data)
-    print(f"Day 9, part 1: {pt_1}")
+    start_time = time.time()
+    pt_1 = puzzle_pt_1(copy.deepcopy(data))
+    print(f"Day 9, part 1 result: {pt_1}")
+    print(f"Day 9, part 1 duration: {time.time() - start_time} s")
+    pt_2 = puzzle_pt_2(copy.deepcopy(data))
     print(f"Day 9, part 2: {pt_2}")
--- tests/advent_2024/test_day_09.py
+++ tests/advent_2024/test_day_09.py
@@ -1,19 +1,41 @@
 import pytest
-from src.advent_of_code.y_2024.day_09.puzzle import puzzle, read_data
+from src.advent_of_code.y_2024.day_09.puzzle import puzzle_pt_1, puzzle_pt_2, read_data
 
 
 @pytest.fixture
-def input_file(tmp_path):
+def input_file_1(tmp_path):
     d = tmp_path / "sub"
     d.mkdir()
-    p = d / "input_test.txt"
+    p = d / "input_test_1.txt"
     p.write_text(
-        """""",
+        """12345""",
         encoding="utf-8",
     )
     return p
 
 
-def test_puzzle_day_09(input_file):
-    data = read_data(input_file)
-    assert puzzle(data) == (0, 0)
+@pytest.fixture
+def input_file_2(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "input_test_2.txt"
+    p.write_text(
+        """2333133121414131402""",
+        encoding="utf-8",
+    )
+    return p
+
+
+def test_puzzle_day_09_1_1(input_file_1):
+    data = read_data(input_file_1)
+    assert puzzle_pt_1(data) == 60
+
+
+def test_puzzle_day_09_1_2(input_file_2):
+    data = read_data(input_file_2)
+    assert puzzle_pt_1(data) == 1928
+
+
+def test_puzzle_day_09_2(input_file_2):
+    data = read_data(input_file_2)
+    assert puzzle_pt_2(data) == 2858
