34c1b8585b75b337bbd7f0b4218c1d4cb761e8e0
Lech Birek <lech.birek@eonnext.com>
2024-12-14 21:05:54 +0000

    Solution day 14 pt. 1

--- src/advent_of_code/y_2024/day_14/puzzle.py
+++ src/advent_of_code/y_2024/day_14/puzzle.py
@@ -6,21 +6,64 @@ def read_data(input_path: str):
     with open(input_path, "r") as inputs:
         input = inputs.readlines()
     data = []
-    behaviour = {}
     for line in input:
-        if "Button A" in line:
-            behaviour["A"] = [int(line.split(",")[0].split("X+")[1]), int(line.split(",")[1].split("Y+")[1])]
-        elif "Button B" in line:
-            behaviour["B"] = [int(line.split(",")[0].split("X+")[1]), int(line.split(",")[1].split("Y+")[1])]
-        elif "Prize" in line:
-            behaviour["P"] = [int(line.split(",")[0].split("X=")[1]), int(line.split(",")[1].split("Y=")[1])]
-            data.append(behaviour)
-            behaviour = {}
+        robot = {}
+        robot["pos"] = [int(line.split(" ")[0][2:].split(",")[0]), int(line.split(" ")[0][2:].split(",")[1])]
+        robot["v"] = [int(line.split(" ")[1][2:].split(",")[0]), int(line.split(" ")[1][2:].split(",")[1])]
+        data.append(robot)
+    # print(data)
     return data
 
 
-def puzzle_pt_1(data) -> int:
-    return 0
+def puzzle_pt_1(data, size, steps) -> int:
+    qc = [0, 0, 0, 0]
+    for i in range(0, steps):
+        for robot in data:
+            new_pos = [robot["pos"][0] + robot["v"][0], robot["pos"][1] + robot["v"][1]]
+            if 0 <= new_pos[0] < size[0]:
+                robot["pos"][0] = new_pos[0]
+            elif new_pos[0] < 0:
+                robot["pos"][0] = size[0] + new_pos[0]
+            elif new_pos[0] >= size[0]:
+                robot["pos"][0] = new_pos[0] - size[0]
+
+            if 0 <= new_pos[1] < size[1]:
+                robot["pos"][1] = new_pos[1]
+            elif new_pos[1] < 0:
+                robot["pos"][1] = size[1] + new_pos[1]
+            elif new_pos[1] >= size[1]:
+                robot["pos"][1] = new_pos[1] - size[1]
+    for robot in data:
+        q = calculate_quadrant(robot["pos"], size)
+        if q is not None:
+            qc[q] += 1
+    return qc[0] * qc[1] * qc[2] * qc[3]
+
+
+def print_map(robots, size):
+    map = [["." for _ in range(0, size[0])] for _ in range(0, size[1])]
+    for robot in robots:
+        if map[robot["pos"][1]][robot["pos"][0]] == ".":
+            map[robot["pos"][1]][robot["pos"][0]] = "1"
+        else:
+            map[robot["pos"][1]][robot["pos"][0]] = str(int(map[robot["pos"][1]][robot["pos"][0]]) + 1)
+    for line in map:
+        string = "".join(line)
+        print(string)
+
+
+def calculate_quadrant(pos, size) -> int:
+    if pos[0] < size[0] / 2 - 1:
+        if pos[1] < size[1] / 2 - 1:
+            return 0
+        elif pos[1] > size[1] / 2:
+            return 2
+    elif pos[0] > size[0] / 2:
+        if pos[1] < size[1] / 2 - 1:
+            return 1
+        elif pos[1] > size[1] / 2:
+            return 3
+    return None
 
 
 def puzzle_pt_2(data) -> int:
@@ -29,8 +72,9 @@ def puzzle_pt_2(data) -> int:
 
 if __name__ == "__main__":
     data = read_data("src/advent_of_code/y_2024/day_14/input.txt")
+    print_map(data, (101, 103))
     start_time = time.time()
-    pt_1 = puzzle_pt_1(copy.deepcopy(data))
+    pt_1 = puzzle_pt_1(copy.deepcopy(data), (101, 103), 100)
     pt_1_duration = time.time() - start_time
     print(f"Day 14, part 1 result: {pt_1}")
     print(f"Day 14, part 1 duration: {pt_1_duration} s")
--- tests/advent_2024/test_day_14.py
+++ tests/advent_2024/test_day_14.py
@@ -8,7 +8,18 @@ def input_file_1(tmp_path):
     d.mkdir()
     p = d / "input_test_1.txt"
     p.write_text(
-        """""",
+        """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3""",
         encoding="utf-8",
     )
     return p
@@ -16,7 +27,7 @@ def input_file_1(tmp_path):
 
 def test_puzzle_day_14_1_1(input_file_1):
     data = read_data(input_file_1)
-    assert puzzle_pt_1(data) == 0
+    assert puzzle_pt_1(data, (11, 7), 100) == 12
 
 
 def test_puzzle_day_14_2_1(input_file_1):
