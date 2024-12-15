7017a9b03731f82300c1b86ee24a44a6164e85b5
Lech Birek <lechb@Lechs-Mac-mini.local>
2024-12-15 13:23:05 +0000

    Solution day 15 pt. 2

--- src/advent_of_code/y_2024/day_15/puzzle.py
+++ src/advent_of_code/y_2024/day_15/puzzle.py
@@ -63,52 +63,142 @@ def move_box(ware, move_dir, pos):
         # Empty space, so now can actually move the box
         ware[next_pos[0]][next_pos[1]] = "O"
         ware[pos[0]][pos[1]] = "."
-        pos = next_pos
         moved = True
     return ware, moved
 
 
-def calculate_gps_coord(ware):
+def calculate_gps_coord(ware, box_edge="O"):
     coords = 0
     for i, row in enumerate(ware):
         for j, col in enumerate(row):
-            if col == "O":
+            if col == box_edge:
                 coords += (100 * i) + j
     return coords
 
 
 def puzzle_pt_2(data) -> int:
-    # DIR = {
-    #     "<": (0, -1),
-    #     "^": (-1, 0),
-    #     ">": (0, 1),
-    #     "v": (1, 0),
-    # }
-    # ware = rebuild_map(data["map"])
-    # pos = data["pos"]
-    # inst = data["inst"]
-    # for move in inst:
-    #     move_dir = DIR[move]
-    #     next_pos = (pos[0] + move_dir[0], pos[1] + move_dir[1])
-    #     if ware[next_pos[0]][next_pos[1]] == "#":
-    #         print("Nothing")
-    #     elif ware[next_pos[0]][next_pos[1]] == ".":
-    #         ware[next_pos[0]][next_pos[1]] = "@"
-    #         ware[pos[0]][pos[1]] = "."
-    #         pos = next_pos
-    #     else:
-    #         print("Box")
-    #         ware, moved = move_box(ware, move_dir, next_pos)
-    #         if moved:
-    #             ware[next_pos[0]][next_pos[1]] = "@"
-    #             ware[pos[0]][pos[1]] = "."
-    #             pos = next_pos
-    # return calculate_gps_coord(ware)
-    return 0
+    DIR = {
+        "<": (0, -1),
+        "^": (-1, 0),
+        ">": (0, 1),
+        "v": (1, 0),
+    }
+    ware, new_y_pos = rebuild_map(data["map"])
+    pos = (data["pos"][0], new_y_pos)
+    inst = data["inst"]
+    for move in inst:
+        move_dir = DIR[move]
+        next_pos = (pos[0] + move_dir[0], pos[1] + move_dir[1])
+        if ware[next_pos[0]][next_pos[1]] == ".":
+            # Empty space, so move the robot in the direction
+            ware[next_pos[0]][next_pos[1]] = "@"
+            ware[pos[0]][pos[1]] = "."
+            pos = next_pos
+        elif ware[next_pos[0]][next_pos[1]] in ["[", "]"]:
+            # Box, so attempt to move it, and if succesful move the robot as well
+            if move in ["<", ">"]:
+                # If we're pushing the box horizontally, we can use similar method to the one in pt.1, with a small modification.
+                ware, moved = move_box_horizontal(ware, move_dir, next_pos)
+            else:
+                # If we're pushing the box vertically, we need to consider that that boxes might overlap. In that case need use a special method :P
+                if ware[next_pos[0]][next_pos[1]] == "[":
+                    pos_l = next_pos
+                    pos_r = (next_pos[0], next_pos[1] + 1)
+                else:
+                    pos_l = (next_pos[0], next_pos[1] - 1)
+                    pos_r = next_pos
+                ware, moved = move_big_box_vertical(ware, move_dir, pos_l, pos_r)
+            if moved:
+                ware[next_pos[0]][next_pos[1]] = "@"
+                ware[pos[0]][pos[1]] = "."
+                pos = next_pos
+    return calculate_gps_coord(ware, "[")
 
 
 def rebuild_map(ware):
-    return ware
+    new_ware = []
+    new_y = -1
+    for row in ware:
+        new_row = []
+        for space in row:
+            if space == "#":
+                new_row.append("#")
+                new_row.append("#")
+            elif space == "O":
+                new_row.append("[")
+                new_row.append("]")
+            elif space == ".":
+                new_row.append(".")
+                new_row.append(".")
+            elif space == "@":
+                new_row.append("@")
+                new_y = len(new_row) - 1
+                new_row.append(".")
+        new_ware.append(new_row)
+    return new_ware, new_y
+
+
+def move_box_horizontal(ware, move_dir, pos):
+    next_pos = (pos[0] + move_dir[0], pos[1] + move_dir[1])
+    moved = False
+    if ware[next_pos[0]][next_pos[1]] in ["[", "]"]:
+        # If the next position is also a box, try moving it and if succesful move current box as well
+        ware, moved = move_box_horizontal(ware, move_dir, next_pos)
+        if moved:
+            ware, moved = move_box_horizontal(ware, move_dir, pos)
+    elif ware[next_pos[0]][next_pos[1]] == ".":
+        # Empty space, so now can actually move the box
+        box_side = ware[pos[0]][pos[1]]
+        ware[next_pos[0]][next_pos[1]] = box_side
+        ware[pos[0]][pos[1]] = "."
+        moved = True
+    return ware, moved
+
+
+def move_big_box_vertical(ware, move_dir, pos_l, pos_r):
+    # pushing boxes vertically, we need to consider pairs of positions, left & right
+    next_pos_l = (pos_l[0] + move_dir[0], pos_l[1] + move_dir[1])
+    next_pos_r = (pos_r[0] + move_dir[0], pos_r[1] + move_dir[1])
+    moved = False
+    if ware[next_pos_l[0]][next_pos_l[1]] == ware[pos_l[0]][pos_l[1]]:
+        # If the next position is also a box with the same side, we can move them together, similar to the horizontal move
+        ware, moved = move_big_box_vertical(ware, move_dir, next_pos_l, next_pos_r)
+        if moved:
+            ware, moved = move_big_box_vertical(ware, move_dir, pos_l, pos_r)
+    elif ware[next_pos_l[0]][next_pos_l[1]] == "." and ware[next_pos_r[0]][next_pos_r[1]] == ".":
+        # Empty space on both sides, so now can actually move the box
+        box_part_l = ware[pos_l[0]][pos_l[1]]
+        box_part_r = ware[pos_r[0]][pos_r[1]]
+        ware[next_pos_l[0]][next_pos_l[1]] = box_part_l
+        ware[next_pos_r[0]][next_pos_r[1]] = box_part_r
+        ware[pos_l[0]][pos_l[1]] = "."
+        ware[pos_r[0]][pos_r[1]] = "."
+        moved = True
+    elif ware[next_pos_l[0]][next_pos_l[1]] != "#" and ware[next_pos_r[0]][next_pos_r[1]] != "#":
+        # Cases where have boxes that aren't vertically aligned
+        if ware[next_pos_l[0]][next_pos_l[1]] == "]" and ware[next_pos_r[0]][next_pos_r[1]] == "[":
+            # 2 boxes, so need to make double move
+            cr_l = ((pos_l[0] + move_dir[0], pos_l[1] - 1 + move_dir[1]), next_pos_l)
+            cr_r = (next_pos_r, (pos_r[0] + move_dir[0], pos_r[1] + 1 + move_dir[1]))
+            ware_t = copy.deepcopy(ware)
+            ware_t, moved_l = move_big_box_vertical(ware_t, move_dir, cr_l[0], cr_l[1])
+            ware_t, moved_r = move_big_box_vertical(ware_t, move_dir, cr_r[0], cr_r[1])
+            # Only actually move if managed to move both of the boxes
+            if moved_l and moved_r:
+                ware = ware_t
+                ware, moved = move_big_box_vertical(ware, move_dir, pos_l, pos_r)
+        # Below is just for cases if we only have 1 not-aligned box on one of the sides
+        elif ware[next_pos_l[0]][next_pos_l[1]] == "]":
+            next_pos_r = (pos_l[0] + move_dir[0], pos_l[1] - 1 + move_dir[1])
+            ware, moved = move_big_box_vertical(ware, move_dir, next_pos_r, next_pos_l)
+            if moved:
+                ware, moved = move_big_box_vertical(ware, move_dir, pos_l, pos_r)
+        else:
+            next_pos_l = (pos_r[0] + move_dir[0], pos_r[1] + 1 + move_dir[1])
+            ware, moved = move_big_box_vertical(ware, move_dir, next_pos_r, next_pos_l)
+            if moved:
+                ware, moved = move_big_box_vertical(ware, move_dir, pos_l, pos_r)
+    return ware, moved
 
 
 if __name__ == "__main__":
--- tests/advent_2024/test_day_15.py
+++ tests/advent_2024/test_day_15.py
@@ -55,6 +55,46 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^""",
     return p
 
 
+@pytest.fixture
+def input_file_3(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "input_test_1.txt"
+    p.write_text(
+        """#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^""",
+        encoding="utf-8",
+    )
+    return p
+
+
+@pytest.fixture
+def input_file_4(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "input_test_1.txt"
+    p.write_text(
+        """#######
+#...#.#
+#.....#
+#..OO.#
+#...O.#
+#...@.#
+#######
+
+^^""",
+        encoding="utf-8",
+    )
+    return p
+
+
 def test_puzzle_day_15_1_1(input_file_1):
     data = read_data(input_file_1)
     assert puzzle_pt_1(data) == 2028
@@ -65,11 +105,16 @@ def test_puzzle_day_15_1_2(input_file_2):
     assert puzzle_pt_1(data) == 10092
 
 
-def test_puzzle_day_15_2_1(input_file_1):
-    data = read_data(input_file_1)
-    assert puzzle_pt_2(data) == 0
+def test_puzzle_day_15_2_2(input_file_2):
+    data = read_data(input_file_2)
+    assert puzzle_pt_2(data) == 9021
 
 
-def test_puzzle_day_15_2_2(input_file_1):
-    data = read_data(input_file_1)
-    assert puzzle_pt_2(data) == 0
+def test_puzzle_day_15_2_3(input_file_3):
+    data = read_data(input_file_3)
+    assert puzzle_pt_2(data) == 618
+
+
+def test_puzzle_day_15_2_4(input_file_4):
+    data = read_data(input_file_4)
+    assert puzzle_pt_2(data) == 822
